0f5c50b310dec5ddd01ecca45a20bcf53821ab36
Viktor Shcherba <vshcherb@amazon.com>
2024-09-26 13:36:19 +0200

    ORiginal impl for diff

--- server/aws-lsp-codewhisperer/src/language-server/utilities/supplementalContextUtil/supplementalContextUtil.ts
+++ server/aws-lsp-codewhisperer/src/language-server/utilities/supplementalContextUtil/supplementalContextUtil.ts
@@ -1,77 +1,68 @@
-// Port of implementation in AWS Toolkit for VSCode
-// https://github.com/aws/aws-toolkit-vscode/blob/9d8ddbd85f4533e539a58e76f7c46883d8e50a79/packages/core/src/codewhisperer/util/supplementalContext/supplementalContextUtil.ts
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+import { fetchSupplementalContextForTest } from './utgUtils'
 import { fetchSupplementalContextForSrc } from './crossFileContextUtil'
 import { isTestFile } from './codeParsingUtil'
+import * as vscode from 'vscode'
+import { CancellationError } from '../../../shared/utilities/timeoutUtils'
+import { ToolkitError } from '../../../shared/errors'
+import { getLogger } from '../../../shared/logger/logger'
 import { CodeWhispererSupplementalContext } from '../../models/model'
-import {
-    CancellationToken,
-    Logging,
-    Position,
-    TextDocument,
-    Workspace,
-} from '@aws/language-server-runtimes/server-interface'
-
-export class CancellationError extends Error {}
 
 export async function fetchSupplementalContext(
-    document: TextDocument,
-    position: Position,
-    workspace: Workspace,
-    logging: Logging,
-    cancellationToken: CancellationToken
+    editor: vscode.TextEditor,
+    cancellationToken: vscode.CancellationToken
 ): Promise<CodeWhispererSupplementalContext | undefined> {
     const timesBeforeFetching = performance.now()
 
-    const isUtg = isTestFile(document.uri, {
-        languageId: document.languageId,
-        fileContent: document.getText(),
+    const isUtg = await isTestFile(editor.document.uri.fsPath, {
+        languageId: editor.document.languageId,
+        fileContent: editor.document.getText(),
     })
 
-    try {
-        let supplementalContextValue:
-            | Pick<CodeWhispererSupplementalContext, 'supplementalContextItems' | 'strategy'>
-            | undefined
+    let supplementalContextPromise: Promise<
+        Pick<CodeWhispererSupplementalContext, 'supplementalContextItems' | 'strategy'> | undefined
+    >
 
-        if (isUtg) {
-            return
-        } else {
-            supplementalContextValue = await fetchSupplementalContextForSrc(
-                document,
-                position,
-                workspace,
-                cancellationToken
-            )
-        }
-
-        if (supplementalContextValue) {
-            return {
-                isUtg: isUtg,
-                isProcessTimeout: false,
-                supplementalContextItems: supplementalContextValue.supplementalContextItems,
-                contentsLength: supplementalContextValue.supplementalContextItems.reduce(
-                    (acc, curr) => acc + curr.content.length,
-                    0
-                ),
-                latency: performance.now() - timesBeforeFetching,
-                strategy: supplementalContextValue.strategy,
-            }
-        } else {
-            return undefined
-        }
-    } catch (err) {
-        if (err instanceof CancellationError) {
-            return {
-                isUtg: isUtg,
-                isProcessTimeout: true,
-                supplementalContextItems: [],
-                contentsLength: 0,
-                latency: performance.now() - timesBeforeFetching,
-                strategy: 'Empty',
-            }
-        } else {
-            logging.log(`Fail to fetch supplemental context for target file ${document.uri}: ${err}`)
-            return undefined
-        }
+    if (isUtg) {
+        supplementalContextPromise = fetchSupplementalContextForTest(editor, cancellationToken)
+    } else {
+        supplementalContextPromise = fetchSupplementalContextForSrc(editor, cancellationToken)
     }
+
+    return supplementalContextPromise
+        .then(value => {
+            if (value) {
+                return {
+                    isUtg: isUtg,
+                    isProcessTimeout: false,
+                    supplementalContextItems: value.supplementalContextItems,
+                    contentsLength: value.supplementalContextItems.reduce((acc, curr) => acc + curr.content.length, 0),
+                    latency: performance.now() - timesBeforeFetching,
+                    strategy: value.strategy,
+                }
+            } else {
+                return undefined
+            }
+        })
+        .catch(err => {
+            if (err instanceof ToolkitError && err.cause instanceof CancellationError) {
+                return {
+                    isUtg: isUtg,
+                    isProcessTimeout: true,
+                    supplementalContextItems: [],
+                    contentsLength: 0,
+                    latency: performance.now() - timesBeforeFetching,
+                    strategy: 'Empty',
+                }
+            } else {
+                getLogger().error(
+                    `Fail to fetch supplemental context for target file ${editor.document.fileName}: ${err}`
+                )
+                return undefined
+            }
+        })
 }
